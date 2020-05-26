dd749512bb19aa9146ae4b18f10103dd1cc9814e
Daniel <daniel.prolite@gmail.com>
2020-05-26 13:48:04 -0500

    Initial Commit

---
+++ Slot Machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by Daniel Vázquez on 26/05/20.
+//  Copyright © 2020 Daniel Vázquez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
