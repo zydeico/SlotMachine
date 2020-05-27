44dcce247a765183b02edc31ff7580d3cff18e06
Daniel <daniel.prolite@gmail.com>
2020-05-26 19:08:45 -0500

    Adding blug effect and pop up functionality

--- Slot Machine/Views/ContentView.swift
+++ Slot Machine/Views/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
     @State private var betAmount: Int = 10
     @State private var isActiveBet10: Bool = true
     @State private var isActiveBet20: Bool = false
+    @State private var showingModal: Bool = false
     
     // MARK: - Functions
     
@@ -68,6 +69,14 @@ struct ContentView: View {
         betAmount = 10
         isActiveBet10 = true
         isActiveBet20 = false
+    }
+    
+    // MARK: - Function if game is Over
+    func isGameOver() {
+        if coins <= 0 {
+            // Show Modal Windows
+            showingModal = true
+        }
     }
     
     // Game is Over
@@ -162,6 +171,8 @@ struct ContentView: View {
                         // Check Wining
                         self.checkWining()
                         
+                        // Game is Over
+                        self.isGameOver()
                     }) {
                         Image("gfx-spin")
                             .renderingMode(.original)
@@ -236,8 +247,68 @@ struct ContentView: View {
             )
                 .padding()
                 .frame(maxWidth: 720)
+                
+                // MARK: - Blur efect
+                .blur(radius: $showingModal.wrappedValue ? 5 : 0, opaque: false)
             
-            // MARK: - PopUp
+            // MARK: - Pop Up
+            if $showingModal.wrappedValue {
+                ZStack {
+                    Color("ColorTransparentBlack")
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    // Modal
+                    VStack(spacing: 0) {
+                        // Title
+                        Text("Game Over")
+                            .font(.system(.title, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(Color("ColorPink"))
+                            .foregroundColor(Color.white)
+                        
+                        Spacer()
+                        
+                        VStack(alignment: .center
+                        , spacing: 16) {
+                            Image("gfx-seven-reel")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 72)
+                            Text("Bad luck! You lost all of the coins. \nLet's play again!")
+                                .font(.system(.body, design: .rounded))
+                                .lineLimit(2)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color.gray)
+                                .layoutPriority(1)
+                            
+                            Button(action: {
+                                self.showingModal = false
+                                self.coins = 100
+                            }) {
+                                Text("New Game".uppercased())
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.semibold)
+                                    .accentColor(Color("ColorPink"))
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 8)
+                                    .frame(minWidth: 128)
+                                    .background(
+                                        Capsule()
+                                            .strokeBorder(lineWidth: 1.75)
+                                            .foregroundColor(Color("ColorPink"))
+                                )
+                            }
+                        }
+                        Spacer()
+                    }
+                    .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight: 320, alignment: .center)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+                }
+            }
         }
         .sheet(isPresented: $showingInfoView) {
             InfoView()
